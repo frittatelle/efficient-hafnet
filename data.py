94b2bb2661afc906e5ddad2f75b7ce7159c3426d
frittatelle <myfavouritefreakelle@gmail.com>
2021-05-16 12:28:40 +0200

    Potsdam patches dataset

--- data.py
+++ data.py
@@ -5,42 +5,13 @@ from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 from PIL import Image
 import numpy as np
-import torchio as tio
-
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-
-class PotsdamRGBDataset(Dataset):
-    """Potsdam TOP RGB dataset"""
-
-    def __init__(self, image_dir, label_dir, transform=None, target_transform=None):
-        self.image_dir = image_dir
-        self.label_dir = label_dir
-        self.transform = transform
-        self.target_transform = target_transform
-        self.images = os.listdir(image_dir)
-        self.labels = os.listdir(label_dir)
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.images[idx])
-        label_path = os.path.join(self.label_dir, self.labels[idx])
-        image = transforms.ToTensor()(np.array(Image.open(img_path)))
-        label = transforms.ToTensor()(np.array(Image.open(label_path)))
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        item = {"image": image, "label": label}
-        return item
 
 class PotsdamDataset(Dataset):
 
     """Potsdam"""
 
-    def __init__(self, rgb_dir, dsm_dir, labels_dir, transform=None, patch_size=128, patch_stride=128):
+    def __init__(self, rgb_dir, dsm_dir, labels_dir, transform=None, patch_size=128, patch_stride=64):
         # list rgb dir, list dsm dir, list label dir
         self.rgb_dir = rgb_dir
         self.dsm_dir = dsm_dir
@@ -53,7 +24,7 @@ class PotsdamDataset(Dataset):
         self.patch_stride = patch_stride
 
     def __len__(self):
-        return len(self.rgb_fns)
+        return self.rgb_fns.__len__()
 
     def __getitem__(self, idx):
         # filenames
@@ -68,7 +39,6 @@ class PotsdamDataset(Dataset):
         rgb_patches = self.extract_patches(rgb, self.patch_size, self.patch_stride)
         dsm_patches = self.extract_patches(dsm, self.patch_size, self.patch_stride, channels=1)
         labels_patches = self.extract_patches(labels, self.patch_size, self.patch_stride)
-        # TODO: apply augmentation
         patches = {
             "id": rgb_fn,
             "rgb_patches": rgb_patches,
@@ -84,20 +54,49 @@ class PotsdamDataset(Dataset):
         patches = torch.reshape(patches, (n_patches, channels, size, size))
         return patches
 
+
+class PotsdamPatchesDataset(Dataset):
+
+    """Potsdam Patches dataset: takes patches as input
+     and apply transformations and stuff"""
+
+    def __init__(self, patches, transform=None, target_transform=None):
+        self.patches = patches
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return self.patches["rgb_patches"].shape[1]
+
+    def __getitem__(self, idx):
+        patch_id = self.patches["id"][0]
+        rgb_patch = self.patches["rgb_patches"][0][idx]
+        dsm_patch = self.patches["dsm_patches"][0][idx]
+        label_patch = self.patches["labels_patches"][0][idx]
+        # TODO: apply augmentation
+        if self.transform:
+            rgb_patch = self.transform(rgb_patch)
+            dsm_patch = self.transform(dsm_patch)
+        if self.target_transform:
+            label_patch = self.target_transform(label_patch)
+        patch = {
+            "id": patch_id,
+            "rgb": rgb_patch,
+            "dsm": dsm_patch,
+            "label": label_patch,
+        }
+        return patch
+
+
 if __name__ == '__main__':
     import time
     start_time = time.time()
-    training_data = PotsdamDataset("data/Potsdam/2_Ortho_RGB", "data/Potsdam/1_DSM", "data/Potsdam/Building_Labels")
-    train_loader = DataLoader(training_data, batch_size=1, shuffle=True)
-    x = next(iter(train_loader))
-    print(x['id'], x['rgb_patches'].shape, x['dsm_patches'].shape, x['labels_patches'].shape)
+    potsdam_dataset = PotsdamDataset("data/Potsdam/2_Ortho_RGB", "data/Potsdam/1_DSM", "data/Potsdam/Building_Labels")
+    potsdam_loader = DataLoader(potsdam_dataset, batch_size=1, shuffle=True)
+    potsdam_patches = next(iter(potsdam_loader))
+
+    potsdam_patches_dataset = PotsdamPatchesDataset(potsdam_patches)
+    potsdam_patches_loader = DataLoader(potsdam_patches_dataset, batch_size=10, shuffle=True)
+    patch = next(iter(potsdam_patches_loader))
+    print(patch['rgb'].shape, patch['dsm'].shape, patch['label'].shape)
     print("--- %s seconds ---" % (time.time() - start_time))
-    # training_data = PotsdamRGBDataset("data/Potsdam/2_Ortho_RGB", "data/Potsdam/Building_Labels")
-    # train_loader = DataLoader(training_data, batch_size=2, shuffle=True)
-    #
-    # batch = next(iter(train_loader))
-    fig, axs = plt.subplots(1, 3)
-    axs[0].imshow(x['rgb_patches'][0][0].permute(1, 2, 0))
-    axs[1].imshow(x['labels_patches'][0][0].permute(1, 2, 0))
-    axs[2].imshow(x['dsm_patches'][0][0].permute(1, 2, 0))
-    plt.show()
